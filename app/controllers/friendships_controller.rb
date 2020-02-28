5a85d9a7616df60a64aeaac7ceea9f388fa651b4
tk0358 <tk0358a@yahoo.co.jp>
2020-02-28 18:50:31 +0900

    Display user/friend profile and tracking option

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,15 +1,24 @@
 class FriendshipsController < ApplicationController
 
   def create
-    friend = User.find(params[:friend])
-    Friendship.create(user_id: current_user.id, friend_id: friend.id)
+    # friend = User.find(params[:friend])
+    # Friendship.create(user_id: current_user.id, friend_id: friend.id)
+    current_user.friendships.build(friend_id: params[:friend])
+    if current_user.save
+      flash[:notice] = "Following friend"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
     redirect_to my_friends_path
+    
   end
 
   def destroy
-    friend = User.find(params[:id])
-    friendship = Friendship.where(user_id: current_user.id, friend_id: friend.id).first
+    # friend = User.find(params[:id])
+    # friendship = Friendship.where(user_id: current_user.id, friend_id: friend.id).first
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
+    flash[:notice] = "Stopped following"
     redirect_to my_friends_path
   end
 end
