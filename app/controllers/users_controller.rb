4997239654436ded0cfa21d46469fbcaf9909390
tk0358 <tk0358a@yahoo.co.jp>
2020-02-28 15:24:40 +0900

    integrate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,38 +12,24 @@ class UsersController < ApplicationController
     @tracked_stocks = @user.stocks
   end
 
-<<<<<<< HEAD
+
   def search
     if params[:friend].present?
       @users = User.search(params[:friend])
       @users = current_user.except_current_user(@users)
       if @users
-=======
-  def search_friend
-    if params[:input].present?
-      @users = User.search_email_or_name(params[:input])
-      if !@users.empty?
->>>>>>> c714828cdda3503135b695811cf31fb906f71fa3
         respond_to do |format|
           format.js { render partial: 'users/friend_result' }
         end
       else
         respond_to do |format|
-<<<<<<< HEAD
           flash.now[:alert] = "Couldn't find user"
-=======
-          flash.now[:alert] = "No users found"
->>>>>>> c714828cdda3503135b695811cf31fb906f71fa3
           format.js { render partial: 'users/friend_result' }
         end
       end
     else
       respond_to do |format|
-<<<<<<< HEAD
         flash.now[:alert] = "Please enter name or email to search"
-=======
-        flash.now[:alert] = "Please input name or email"
->>>>>>> c714828cdda3503135b695811cf31fb906f71fa3
         format.js { render partial: 'users/friend_result' }
       end
     end
